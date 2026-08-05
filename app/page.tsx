72a3cecffd0874da2c5d7bf595bfa02ec0eb6866
manopriyan6 <manopriyan33@gmail.com>
2026-08-05 10:22:04 +0530

    Remove StatsSection from home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/hero/HeroSection";
 import ServicesSection from "@/components/sections/ServicesSection";
 import ProductsSection from "@/components/sections/ProductsSection";
-import StatsSection from "@/components/sections/StatsSection";
+
 import TechnologiesSection from "@/components/sections/TechnologiesSection";
 import WhyMavtopSection from "@/components/sections/WhyMavtopSection";
 import ContactCTASection from "@/components/sections/ContactCTASection";
@@ -15,7 +15,7 @@ export default function Home() {
       <HeroSection />
       <ServicesSection />
       <ProductsSection />
-      <StatsSection />
+
       <TechnologiesSection />
       <WhyMavtopSection />
       <ContactCTASection />
